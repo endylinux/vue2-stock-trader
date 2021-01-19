5af30e5b7a7af980795f875fd65c925f8b1ae06b
Eugen-Alexander Habrony <eugen-alexander.habrony@ho2-gmbh.de>
2021-01-19 02:10:48 +0100

    Added routing and created routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import VueRouter from 'vue-router'
+import { routes } from "@/routes";
 
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   el:'#app',
+  router,
   render: h => h(App)
 })
 // new Vue({
